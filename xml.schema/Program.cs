8424e55843b29a6d9ff0c0265ef5480588ed5d0f
bus1hero <petru.cervac@gmail.com>
2022-08-31 13:46:03 +0300

    chore: remove useless as your mom comments

--- xml.schema/Program.cs
+++ xml.schema/Program.cs
@@ -3,17 +3,14 @@ using xml.schema.Services.Impl;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<ISchemaService, InMemorySchemaService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
