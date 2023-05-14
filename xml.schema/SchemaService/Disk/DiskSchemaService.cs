5215e96a1dd06c1b12b9678a67acb27549f9d2de
bus1hero <petru.cervac@gmail.com>
2023-05-14 12:09:58 +0300

    feat: switch to on-disck schema service

---
+++ xml.schema/SchemaService/Disk/DiskSchemaService.cs
@@ -0,0 +1,21 @@
+﻿namespace xml.schema.SchemaService.Disk;
+
+internal sealed class DiskSchemaService : ISchemaService
+{
+    private readonly Dictionary<string, string> _namespaces;
+    public DiskSchemaService() => _namespaces = new Dictionary<string, string>
+    {
+        ["http://schemas.microsoft.com/powershell/help/2010/05"] = "HelpInfo",
+    };
+
+    public string GetSchema(string xmlNamespace)
+    {
+        if (!_namespaces.TryGetValue(xmlNamespace, out var filename))
+        {
+            return "Namespace not found";
+        }
+        var path = $@"resources\schemas\{filename}.xsd";
+        var text = File.ReadAllText(path);
+        return text;
+    }
+}
