518ee1227813c1ad308dd73b471c9634629963ac
Cervac Petru <petru.cervac@gmail.com>
2023-05-14 11:09:01 +0300

    build: configure nuke (#4)

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,39 @@
-using System;
-using System.Linq;
+using System.Diagnostics.CodeAnalysis;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+namespace _build;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(_ => _
+                .EnableNoLogo());
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore();
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(_ => _
+                .EnableNoLogo()
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
         });
-
 }
