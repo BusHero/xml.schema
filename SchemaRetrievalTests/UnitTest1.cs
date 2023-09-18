d5132833d79e86bfb0449868dda25637cadde133
bus1hero <petru.cervac@gmail.com>
2023-09-18 23:25:18 +0300

    ci: fix integration tests

--- SchemaRetrievalTests/UnitTest1.cs
+++ SchemaRetrievalTests/UnitTest1.cs
@@ -15,33 +15,28 @@ public class UnitTest1 : IAsyncLifetime
         tokenSource = new CancellationTokenSource();
     }
     
-    [Fact] public void Test1()
+    [Fact] public async Task Test1()
     {
-        true.Should().BeFalse();
-        // var client = new HttpClient();
-        // client.BaseAddress = new Uri("http://localhost:5160");
-        // var result = await client.GetAsync("greet");
-        //
-        // result.EnsureSuccessStatusCode();
-        //
-        // var response = await result.Content.ReadAsStringAsync();
-        //
-        // response.Should().Be("Hello, World!");
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("http://localhost:5000");
+        var result = await client.GetAsync("greet");
+        
+        result.EnsureSuccessStatusCode();
+        
+        var response = await result.Content.ReadAsStringAsync();
+        
+        response.Should().Be("Hello, World!");
     }
     
     public Task InitializeAsync()
     {
-        return Task.CompletedTask;
-        // return Task.Run(() => Cli.Wrap("dotnet")
-        //     .WithArguments(args => args
-        //         .Add("run")
-        //         .Add("--no-build")
-        //         .Add("--project")
-        //         .Add("XmlSchemaApi"))
-        //     .WithWorkingDirectory(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", ".."))
-        //     .WithStandardOutputPipe(PipeTarget.ToDelegate(outputHelper.WriteLine))
-        //     .WithStandardErrorPipe(PipeTarget.ToDelegate(outputHelper.WriteLine))
-        //     .ExecuteAsync(tokenSource.Token));
+        return Task.Run(() => Cli.Wrap("dotnet")
+            .WithArguments(args => args
+                .Add(Path.Combine("publish", "XmlSchemaApi.dll")))
+            .WithWorkingDirectory(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", ".."))
+            .WithStandardOutputPipe(PipeTarget.ToDelegate(outputHelper.WriteLine))
+            .WithStandardErrorPipe(PipeTarget.ToDelegate(outputHelper.WriteLine))
+            .ExecuteAsync(tokenSource.Token));
     }
     
     public Task DisposeAsync()
