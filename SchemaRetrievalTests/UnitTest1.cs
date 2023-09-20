ce20734deb0b4f3bd696e3146d6a937903cc223e
bus1hero <petru.cervac@gmail.com>
2023-09-20 23:25:10 +0300

    asd

--- SchemaRetrievalTests/UnitTest1.cs
+++ SchemaRetrievalTests/UnitTest1.cs
@@ -12,20 +12,27 @@ public class UnitTest1 : IAsyncLifetime
     public UnitTest1(ITestOutputHelper outputHelper)
     {
         this.outputHelper = outputHelper;
-        tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+        tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(60));
     }
     
     [Fact] public async Task Test1()
     {
-        var client = new HttpClient();
-        client.BaseAddress = new Uri("http://localhost:5000");
-        var result = await client.GetAsync("greet");
-        
-        result.EnsureSuccessStatusCode();
-        
-        var response = await result.Content.ReadAsStringAsync();
-        
-        response.Should().Be("Hello, World!");
+        await Cli.Wrap("dotnet")
+            .WithArguments(args => args
+                .Add(Path.Combine("publish", "XmlSchemaApi.dll")))
+            .WithWorkingDirectory("/home/runner/work/xml.schema/xml.schema")
+            .WithStandardOutputPipe(PipeTarget.ToDelegate(outputHelper.WriteLine))
+            .WithStandardErrorPipe(PipeTarget.ToDelegate(outputHelper.WriteLine))
+            .ExecuteAsync(tokenSource.Token);
+        // var client = new HttpClient();
+        // client.BaseAddress = new Uri("http://localhost:5000");
+        // var result = await client.GetAsync("greet");
+        //
+        // result.EnsureSuccessStatusCode();
+        //
+        // var response = await result.Content.ReadAsStringAsync();
+        //
+        // response.Should().Be("Hello, World!");
     }
     
     public Task InitializeAsync()
